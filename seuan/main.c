8d671a8c74a536921c864ec419a45079f57b7f54
seungb5 <seungbo1112@gmail.com>
2021-07-28 11:31:53 +0900

    구조체 수정.

--- seuan/main.c
+++ seuan/main.c
@@ -3,6 +3,7 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<sys/time.h>
 
 int	ft_strlen(char *str)
 {
@@ -38,31 +39,43 @@ int	ft_atoi(const char *str)
 	return ((int)(result * sign));
 }
 
+
 // philosopher 구조체
-typedef struct s_philo_info
+
+typedef struct s_info	t_info;
+typedef struct s_philo	t_philo;
+
+struct s_info
 {   
     int philo_id;
-    int num_of_philo;
-    int time_to_die;
-    int time_to_eat;
-    int time_to_sleep;
-    int num_of_eating;
-}   t_philo_info;
+    int num_philo;
+    int time_die;
+    int time_eat;
+    int time_sleep;
+    int must_eating;
+    t_philo *philo;
+};
 
-void    init_info(t_philo_info *info, char **argv)
+struct s_philo
+{
+    int stt_time;
+    t_info *info;
+};
+
+void    init_info(t_info *info, char **argv)
 {
     info->philo_id = 1; // 철학자의 이름은 1부터 시작한다.
-    info->num_of_philo = ft_atoi(argv[1]);
-    info->time_to_die = ft_atoi(argv[2]);
-    info->time_to_eat = ft_atoi(argv[3]);
-    info->time_to_sleep = ft_atoi(argv[4]);
+    info->num_philo = ft_atoi(argv[1]);
+    info->time_die = ft_atoi(argv[2]);
+    info->time_eat = ft_atoi(argv[3]);
+    info->time_sleep = ft_atoi(argv[4]);
     if (argv[5])
-        info->num_of_eating = ft_atoi(argv[5]);
+        info->must_eating = ft_atoi(argv[5]);
 }
 
 void *philosopher(void *arg)
 {
-    t_philo_info info = *(t_philo_info *)arg;
+    t_info info = *(t_info *)arg;
     printf("thread %d started\n", info.philo_id);
 
     // thread가 실행되는 가상의 시간 설정
@@ -80,16 +93,16 @@ int main(int argc, char **argv)
 {
     int i = 0;
     int err;
-    t_philo_info info;
+    t_info info;
     pthread_t *tid; // thread 생성 시 고유의 thread id 값을 가지게 된다.
 
     // 올바른 인자 수 여부 체크.
     if (!(argc == 5 || argc == 6))
         return (0);
     init_info(&info, argv);
-    tid = (pthread_t *)malloc(sizeof(pthread_t) * (info.num_of_philo));
+    tid = (pthread_t *)malloc(sizeof(pthread_t) * (info.num_philo));
     // pthread_create thread 생성
-    while (info.philo_id < info.num_of_philo)
+    while (info.philo_id < info.num_philo)
     {
         err = pthread_create(&(tid[i]), NULL, &philosopher, &info);
         if (err != 0)
@@ -99,7 +112,7 @@ int main(int argc, char **argv)
     }
 
     // pthread_join
-    while (i < info.num_of_philo)
+    while (i < info.num_philo)
         pthread_join(tid[i++], NULL);
     return 0;
 }
