ef86b5eb51393f742b783561a828f94f61b7f05d
seungb5 <seungbo1112@gmail.com>
2021-08-03 03:20:58 +0900

    무택스.

--- seuan/main.c
+++ seuan/main.c
@@ -1,9 +1,17 @@
+// header
 #include<stdio.h>
 #include<string.h>
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/time.h>
+
+// define
+# define FORK 1
+# define EAT 2
+# define SLEEP 3
+# define THINK 4
+# define DIE 5
 
 int	ft_strlen(char *str)
 {
@@ -47,72 +55,194 @@ typedef struct s_philo	t_philo;
 
 struct s_info
 {   
-    int philo_id;
     int num_philo;
     int time_die;
     int time_eat;
     int time_sleep;
     int must_eating;
+    int init_time;
+    int dead;
+    pthread_mutex_t status;
+    pthread_mutex_t *fork;
     t_philo *philo;
 };
 
 struct s_philo
 {
-    int stt_time;
+    int id;
+    int stt;
+    int fork_l;
+    int fork_r;
     t_info *info;
+    pthread_t thread;
+    pthread_t monitor;
+    pthread_mutex_t philo_lock;
 };
+
+int	current_time(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
 
 void    init_info(t_info *info, char **argv)
 {
-    info->philo_id = 1; // 철학자의 이름은 1부터 시작한다.
     info->num_philo = ft_atoi(argv[1]);
     info->time_die = ft_atoi(argv[2]);
     info->time_eat = ft_atoi(argv[3]);
     info->time_sleep = ft_atoi(argv[4]);
+    info->dead = 0;
+    info->init_time = 0;
     if (argv[5])
         info->must_eating = ft_atoi(argv[5]);
+    pthread_mutex_init(&info->status, NULL);
+}
+
+void    init_fork(t_info *info)
+{
+    int i;
+
+    info->fork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * info->num_philo);
+    i = 0;
+    while (i < info->num_philo)
+    {
+        pthread_mutex_init(&info->fork[i], NULL);
+        i++;
+    }
+}
+
+void    init_philo(t_info *info)
+{
+    int i;
+
+    info->philo = (t_philo *)malloc(sizeof(t_philo) * info->num_philo);
+    i = 0;
+    while (i < info->num_philo)
+    {
+        info->philo[i].id = i;
+        info->philo[i].info = info;
+        info->philo[i].stt = 0;
+        info->philo[i].fork_l = i;
+        info->philo[i].fork_r = (i + 1) % info->num_philo;
+        pthread_mutex_init(&info->philo[i].philo_lock, NULL);
+        i++;
+    }
+}
+
+void    print_status(t_philo *philo, int status)
+{
+    // Befort print, mutex lock thread
+    pthread_mutex_lock(&philo->info->status);
+
+    // timestamp in ms
+    printf("[%dms] ", current_time() - philo->info->init_time);
+
+    // thread num
+    printf("%d philosopher ", philo->id + 1);
+    
+    // msg
+    if (status == FORK)
+        printf("has taken a fork\n");
+    else if (status == EAT)
+        printf("is eating\n");
+    else if (status == SLEEP)    
+        printf("is sleeping\n");
+    else if (status == THINK)
+        printf("is thinking\n");
+    else if (status == DIE)
+        printf("died\n");
+    pthread_mutex_unlock(&philo->info->status);
 }
 
 void *philosopher(void *arg)
 {
-    t_info info = *(t_info *)arg;
-    printf("thread %d started\n", info.philo_id);
+    t_philo *philo;
+
+    // thread 생성 시 넘어오는 매개변수 받기
+    philo = arg;
+
+    // 짝수 thread에 대해 usleep을 지정함으로써 홀수 thread가 우선적으로 실행되게 함.
+    if (philo->id % 2 == 0)
+        usleep(philo->info->time_eat);
 
     // thread가 실행되는 가상의 시간 설정
-    int i = 0;
-    while (i++ < 5)
+    while (!philo->info->dead)
     {
-        printf("%d thread: %d\n", info.philo_id, i);
-        sleep(1);
+        pthread_mutex_lock(&philo->info->fork[philo->fork_l]);
+        print_status(philo, FORK);
+        pthread_mutex_lock(&philo->info->fork[philo->fork_r]);
+        print_status(philo, FORK);
+        print_status(philo, EAT);
+        usleep(1000 * philo->info->time_eat);
+        pthread_mutex_unlock(&philo->info->fork[philo->fork_l]);
+        pthread_mutex_unlock(&philo->info->fork[philo->fork_r]);
+        print_status(philo, SLEEP);
+        usleep(1000 * philo->info->time_sleep);
+        print_status(philo, THINK);
     }
-    printf("thread %d finished\n", info.philo_id);
+    return NULL;
+}
+
+void    *monitor(void *arg)
+{
+    t_philo *philo;
+
+    philo = arg;
+    // philo lock은 왜 하는 지 모르겠음...
+    pthread_mutex_lock(&philo->philo_lock);
+    while (!philo->info->dead)
+    {
+        if (current_time() - philo->stt >= philo->info->time_eat)
+        {
+            philo->info->dead = 1;
+            print_status(philo, DIE);
+            break;
+        }
+    }
+    pthread_mutex_unlock(&philo->philo_lock);
     return NULL;
 }
 
 int main(int argc, char **argv)
 {
     int i = 0;
-    int err;
     t_info info;
-    pthread_t *tid; // thread 생성 시 고유의 thread id 값을 가지게 된다.
 
     // 올바른 인자 수 여부 체크.
     if (!(argc == 5 || argc == 6))
         return (0);
     init_info(&info, argv);
-    tid = (pthread_t *)malloc(sizeof(pthread_t) * (info.num_philo));
-    // pthread_create thread 생성
-    while (info.philo_id < info.num_philo)
-    {
-        err = pthread_create(&(tid[i]), NULL, &philosopher, &info);
-        if (err != 0)
-            printf("\ncan't create thread :[%s]", strerror(err));
-        usleep(100);
-        info.philo_id++;
-    }
 
-    // pthread_join
+    // mutex init fork
+    init_fork(&info);
+
+    // init philo
+    init_philo(&info);
+
+    // pthread_create thread
+    info.init_time = current_time();
+    i = 0;
     while (i < info.num_philo)
-        pthread_join(tid[i++], NULL);
+    {
+        info.philo[i].stt = current_time();
+        if (pthread_create(&(info.philo[i].thread), NULL, &philosopher, &info.philo[i]))
+            return (printf("Failed create thread\n"));
+        if (pthread_create(&(info.philo[i].monitor), NULL, &monitor, &info.philo[i]))
+            return (printf("Failed create thread\n"));
+        usleep(100);
+        i++;
+    } 
+    // pthread_join
+    i = 0;
+    while (i < info.num_philo)
+    {
+        if (pthread_join(info.philo[i].thread, NULL))
+            return (printf("Failed to join thread.\n"));
+        if (pthread_join(info.philo[i].monitor, NULL))
+            return (printf("Failed to join thread.\n"));
+        i++;
+    }
     return 0;
 }
