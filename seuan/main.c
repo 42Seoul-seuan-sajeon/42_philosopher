fdc8f49f192b3e6498dc975914744578a47dcc15
seungb5 <seungbo1112@gmail.com>
2021-08-06 02:01:41 +0900

    함수 줄 수 수정 완료.

--- seuan/main.c
+++ seuan/main.c
@@ -6,32 +6,29 @@
 /*   By: seuan <seuan@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/06 00:11:28 by seuan             #+#    #+#             */
-/*   Updated: 2021/08/06 00:45:26 by seuan            ###   ########.fr       */
+/*   Updated: 2021/08/06 02:01:13 by seuan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void    set_info(t_info *info, char **argv)
+{
+    init_info(&info, argv);
+    init_fork(&info);
+    init_philo(&info);
+}
+
 int main(int argc, char **argv)
 {
-    int i = 0;
+    int i;
     t_info info;
 
-    // 올바른 인자 수 여부 체크.
     if (!(argc == 5 || argc == 6))
         return (0);
-    init_info(&info, argv);
-
-    // mutex init fork
-    init_fork(&info);
-
-    // init philo
-    init_philo(&info);
-
-    // pthread_create thread
     info.init_time = current_time();
-    i = 0;
-    while (i < info.num_philo)
+    i = -1;
+    while (++i < info.num_philo)
     {
         info.philo[i].th_time = current_time();
         if (pthread_create(&(info.philo[i].thread), NULL, &philosopher, &info.philo[i]))
@@ -39,17 +36,14 @@ int main(int argc, char **argv)
         if (pthread_create(&(info.philo[i].monitor), NULL, &monitor, &info.philo[i]))
             return (printf("Failed create thread\n"));
         usleep(100);
-        i++;
-    } 
-    // pthread_join
-    i = 0;
-    while (i < info.num_philo)
+    }
+    i = -1;
+    while (++i < info.num_philo)
     {
         if (pthread_join(info.philo[i].thread, NULL))
             return (printf("Failed to join thread.\n"));
         if (pthread_join(info.philo[i].monitor, NULL))
             return (printf("Failed to join thread.\n"));
-        i++;
     }
     return 0;
 }
--- seuan/print.c
+++ seuan/print.c
@@ -6,7 +6,7 @@
 /*   By: seuan <seuan@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/06 00:33:38 by seuan             #+#    #+#             */
-/*   Updated: 2021/08/06 00:33:57 by seuan            ###   ########.fr       */
+/*   Updated: 2021/08/06 01:35:10 by seuan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,14 @@
 
 void    print_th_status(t_philo *philo, int th_status)
 {
-    // Befort print, mutex lock thread
     pthread_mutex_lock(&philo->info->th_status);
     if (philo->info->th_is_dead)
     {
         pthread_mutex_unlock(&philo->info->th_status);
         return;
     }
-
-    // timestamp in ms
     printf("[%dms] ", current_time() - philo->info->init_time);
-
-    // thread num
     printf("%d philosopher ", philo->id + 1);
-    
-    // msg
     if (th_status == FORK)
         printf("has taken a fork\n");
     else if (th_status == EAT)
